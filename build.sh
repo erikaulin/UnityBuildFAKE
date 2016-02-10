2064063e555ecdab9bf04529be3ab202eb786ffa
Erik Aulin <erik@aulin.co>
2016-02-10 18:05:40 +0100

    full path for mono

--- build.sh
+++ build.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-
-mono --runtime=v4.0 .nuget/nuget.exe install FAKE -Version 4.3.7
-mono --runtime=v4.0 packages/FAKE.4.3.7/tools/FAKE.exe build.fsx $@
+/usr/local/bin/mono --runtime=v4.0 .nuget/nuget.exe install FAKE -Version 4.3.7
+/usr/local/bin/mono --runtime=v4.0 packages/FAKE.4.3.7/tools/FAKE.exe build.fsx $@
